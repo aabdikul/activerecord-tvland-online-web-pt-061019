57c5e44ce674ea432f73e79abda7480940ad2627
Aida Abdikulova <abdikulova@gmail.com>
2019-10-04 03:54:34 +0000

    Automatically backed up by Learn

--- app/models/network.rb
+++ app/models/network.rb
@@ -3,6 +3,6 @@ class Network < ActiveRecord::Base
   has_many :characters, through: :shows
 
   def sorry
-    return "We're sorry about passing on John Mulaney's Pilot"
+    return "We're sorry about passing on John Mulaney's pilot"
   end
 end
